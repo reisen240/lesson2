47ed8e689c0f5028b0d3251048ba0859ec813ebf
nishio <1198728ckd@gmail.com>
2019-04-25 13:03:01 +0000

    Finish static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
   # ページごとにタイトルを返す。
-  def full_title(page_title = "")　#　 メソッド名と因数の定義
-    base_title = "勤怠管理システム"#変数に代入
+  def full_title(page_title = "")
+    base_title = "勤怠管理システム"
     if page_title.empty?
       base_title
     else
